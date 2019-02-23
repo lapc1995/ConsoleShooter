b25c59535513c4c26b6fae629696eddd37744513
Luís Carvalho <lapc19@hotmail.com>
2019-02-23 23:33:00 +0000

    Updates

---
+++ precache-manifest.9a12b10c36c030a718a541608f9fefc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ConsoleShooter/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "161332adc15b41d01b72",
+    "url": "/ConsoleShooter/static/js/runtime~main.161332ad.js"
+  },
+  {
+    "revision": "15fdedfacde7ce20c266",
+    "url": "/ConsoleShooter/static/js/main.15fdedfa.chunk.js"
+  },
+  {
+    "revision": "bc1075079f11b9112646",
+    "url": "/ConsoleShooter/static/js/2.bc107507.chunk.js"
+  },
+  {
+    "revision": "15fdedfacde7ce20c266",
+    "url": "/ConsoleShooter/static/css/main.7a28ec70.chunk.css"
+  },
+  {
+    "revision": "66d0bb0b6cc95f8c95b89785ccc61c5a",
+    "url": "/ConsoleShooter/index.html"
+  }
+];
